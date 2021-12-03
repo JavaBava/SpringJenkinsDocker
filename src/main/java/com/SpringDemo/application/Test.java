c187051462110a35e0ce43f77f006e5e1b49e387
JavaBava <mr.ashutosh28@gmail.com>
2021-12-03 12:49:27 +0530

    third commit

--- src/main/java/com/SpringDemo/application/Test.java
+++ src/main/java/com/SpringDemo/application/Test.java
@@ -3,6 +3,7 @@ package com.SpringDemo.application;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Jenkins ");
+		System.out.println("Hello Jenkins Namaste");
+		
 	}
 }
